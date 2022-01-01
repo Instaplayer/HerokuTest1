c83f7859e2ab4519af742fe2c375921fa8f848fb
Jakub <kuba.proszek@gmail.com>
2022-01-01 16:21:58 +0100

    Zmiany w strukturze serwera

--- server.js
+++ server.js
@@ -4,7 +4,28 @@ const PORT = process.env.PORT || 3000;
 // bardzo istotna linijka - port zostaje przydzielony przez Heroku
 
 app.get("/", function (req, res) {
-    res.send("<h1>my app on heroku!</h1>")
+    
+    codeString ="<h1>my app on heroku!</h1>"
+    codeString += `<p style="color: #CCCCCC">autor: Jakub Proszek</p>`
+
+    res.send(codeString)
+})
+
+app.get("/data", function (req, res) {
+    
+    codeString ="<h1>my app on heroku!</h1>"
+
+    infoDataObject = {}
+
+    infoDataObject.imie = req.query.imie
+    infoDataObject.nazwisko = req.query.nazwisko
+    infoDataObject.klasa = req.query.klasa.substr(0, 3)
+    infoDataObject.grupa = req.query.klasa.substr(3, 1)
+
+    codeString += "<p>" + JSON.stringify(infoDataObject) + "</p>"
+    codeString += `<p style="color: #CCCCCC">autor: Jakub Proszek</p>`
+
+    res.send(codeString)
 })
 
 app.listen(PORT, function () {
